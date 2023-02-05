c437e42e8c37909e150839ebb68d5a621962f564
franco_bertinatti <franbertinatti@gmail.com>
2023-02-05 04:31:27 -0300

    armando modal para confirmpurchase

--- src/containers/CartContainer/index.jsx
+++ src/containers/CartContainer/index.jsx
@@ -1,30 +1,38 @@
 import React, { useContext } from "react";
 import { Shop } from "../../contexts/ShopProvider";
+import generateOrderObject from "../../services/generateOrderObject";
 import TableRow from "./TableRow";
 
 const Cart = () => {
   const { products } = useContext(Shop);
 
+  const confirmarCompraCarrito = () => {
+    const order = generateOrderObject({});
+  };
+
   return (
-    <div classNameName="p-3">
-      <table className="table table-hover">
-        <thead className="table-dark">
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col"></th>
-            <th scope="col">Producto</th>
-            <th scope="col">Precio</th>
-            <th scope="col">Cantidad</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => {
-            return <TableRow key={product.id} product={product} />;
-          })}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div classNameName="p-3">
+        <table className="table table-hover">
+          <thead className="table-dark">
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col"></th>
+              <th scope="col">Producto</th>
+              <th scope="col">Precio</th>
+              <th scope="col">Cantidad</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => {
+              return <TableRow key={product.id} product={product} />;
+            })}
+          </tbody>
+        </table>
+      </div>
+      <button onClick={confirmarCompraCarrito}>Confirmar compra</button>
+    </>
   );
 };
 
